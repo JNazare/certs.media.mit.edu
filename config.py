7c22504f4aff8baeae2169efb5f6172df233c0d1
jnazare <jules.nazare@gmail.com>
2015-09-04 09:37:56 -0500

    formatting

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ HASHMAP_PATH = DATA_PATH + 'hash_id_mappings.json'
 PUBKEYMAP_PATH = DATA_PATH + 'pubkey_id_mappings.json'
 BLOCKCHAIN_ADDRESS = '1PMcrASk9yqohXmoBtS4SjoCauN5xGqdkL'
 CERT_MARKER = 'LABCDMX'
-RECENTLY_ADDED = []
+RECENTLY_ADDED = ['14vtoAR2iAANL1dVhRfd9bmpJA3PN6bbgw', '145VMccSzbxe9QP3JQm7CR6mrbvzpB25n2', '12syyZjM6tDUDAorTQDr1tteq27Yux3gf8']
